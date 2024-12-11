4b940143d40e006920056130141fde4cfa2a4673
rabbit1rabbit <147872988+rabbit1rabbit@users.noreply.github.com>
2024-12-11 05:06:36 -0700

    Completed Image slider

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//Image Gallery Slider
+
 const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
 let intervalId = null;
@@ -5,21 +7,36 @@ let intervalId = null;
 initializeSlider();
 
 function initializeSlider(){
+
     slides[slideIndex].classList.add("displaySlide")
     intervalId = setInterval(nextSlide, 5000);
-    console.log(intervalId)
 
 
 }
 
 function showSlide(index){
-
+    
+    if (index >= slides.length) {
+        slideIndex = 0;
+        
+    }
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
+        
+    }
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
 }
 
 function prevSlide(){
-
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
 }
 
 function nextSlide(){
-    
+    slideIndex++;
+    showSlide(slideIndex);
 }
